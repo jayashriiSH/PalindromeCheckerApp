b252d8210cb677ab2d1401a09c7b2a24d8ace23a
jayashriiSH <pcmj.jayashriishankar@gmail.com>
2026-03-05 10:03:00 +0530

    UC4:Array Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,13 +1,19 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "madam";
-        String reversed = "";
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
+        String input = "radar";
+        char[] characters = input.toCharArray();
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = characters.length - 1;
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
-        boolean isPalindrome = input.equals(reversed);
         System.out.println("Input text: " + input);
-        System.out.println("Reversed text: " + reversed);
         System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
