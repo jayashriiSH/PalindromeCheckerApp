30e5190f371c8a961406c2ebdd8fc676b643cab7
jayashriiSH <pcmj.jayashriishankar@gmail.com>
2026-03-12 08:53:57 +0530

    UC9 implementation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,36 @@
+
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== Recursive Palindrome Checker ===");
+        System.out.print("Enter a string: ");
+
+        String input = scanner.nextLine();
+        String processed = input.replaceAll("\\s+", "").toLowerCase();
+
+        boolean result = isPalindrome(processed, 0, processed.length() - 1);
+
+        if (result) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        scanner.close();
+    }
 }
